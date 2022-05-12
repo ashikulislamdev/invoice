cedc4e7a445b29180a69a20b63f6f18fd23ebf61
saun2648 <ashikulislamsawan@gmail.com>
2022-05-12 13:23:48 +0600

    Dashboard total card added with functionallity

--- views/dashboard-view.php
+++ views/dashboard-view.php
@@ -1,23 +1,67 @@
+<?php 
+
+    if(!isset($current_user_id)){die('Unauthorized Error');}
+
+    //summation of loan amount from loan
+    $getSumLoanQry = "SELECT SUM(amount) AS sumLoan FROM `loan`";
+    $getSumLoan = mysqli_query($conn, $getSumLoanQry);
+    $getSumLoan = mysqli_fetch_assoc($getSumLoan);
+    $getSumLoan = $getSumLoan['sumLoan'];
+    
+    //summation of pay amount from invoices
+    $getSumPayQry = "SELECT SUM(pay) AS sumPay FROM `invoices`";
+    $getSumPay = mysqli_query($conn, $getSumPayQry);
+    $getSumPay = mysqli_fetch_assoc($getSumPay);
+    $getSumPay = $getSumPay['sumPay'];
+
+    //total income
+    $all_incomes = $getSumLoan + $getSumPay;
+
+    //last month income from loan
+    $getLastMonthLoan = mysqli_query($conn, "SELECT SUM(amount) AS lastMonthLoan FROM `loan` WHERE date > (NOW() - INTERVAL 1 MONTH)");
+    $getLastMonthLoan = mysqli_fetch_assoc($getLastMonthLoan);
+    $getLastMonthLoan = $getLastMonthLoan['lastMonthLoan'];
+
+    $getLastMonthPay = mysqli_query($conn, "SELECT SUM(pay) AS lastMonthPay FROM `invoices` WHERE created > (NOW() - INTERVAL 1 MONTH)");
+    $getLastMonthPay = mysqli_fetch_assoc($getLastMonthPay);
+    $getLastMonthPay = $getLastMonthPay['lastMonthPay'];
+    
+    $totalLastMonthIncome = $getLastMonthLoan + $getLastMonthPay;
+
+
+
+    //summation of cost amount
+    $getSumCost = mysqli_query($conn, "SELECT SUM(amount) AS sumCost FROM `cost`");
+    $getSumCost = mysqli_fetch_assoc($getSumCost);
+    $getSumCost = $getSumCost['sumCost'];
+    
+    //last month cost amount
+    $getLastMonthCost = mysqli_query($conn, "SELECT SUM(amount) AS lastMonthCost FROM `cost` WHERE date > (NOW() - INTERVAL 1 MONTH)");
+    $getLastMonthCost = mysqli_fetch_assoc($getLastMonthCost);
+    $getLastMonthCost = $getLastMonthCost['lastMonthCost'];
+
+?>
+
 <div class="row">
 <div class="col-md-6 col-xl-3">
         <div class="card bg-c-blue order-card">
             <div class="card-block">
-                <h6 class="m-b-20">Orders Received</h6>
-                <h2 class="text-right"><i class="ti-shopping-cart f-left"></i><span>486</span></h2>
-                <p class="m-b-0">Completed Orders<span class="f-right">351</span></p>
+                <h6 class="m-b-20">Total Income</h6>
+                <h2 class="text-right"><i class="ti-shopping-cart f-left"></i><span><?php echo $all_incomes; ?></span></h2>
+                <p class="m-b-0">This Month<span class="f-right"><?php echo $totalLastMonthIncome; ?></span></p>
             </div>
         </div>
     </div>
     <div class="col-md-6 col-xl-3">
         <div class="card bg-c-green order-card">
             <div class="card-block">
-                <h6 class="m-b-20">Total Sales</h6>
-                <h2 class="text-right"><i class="ti-tag f-left"></i><span>1641</span></h2>
-                <p class="m-b-0">This Month<span class="f-right">213</span></p>
+                <h6 class="m-b-20">Total Expense</h6>
+                <h2 class="text-right"><i class="ti-tag f-left"></i><span><?php echo $getSumCost; ?></span></h2>
+                <p class="m-b-0">This Month<span class="f-right"><?php echo $getLastMonthCost; ?></span></p>
             </div>
         </div>
     </div>
-    <div class="col-md-6 col-xl-3">
+    <!-- <div class="col-md-6 col-xl-3">
         <div class="card bg-c-yellow order-card">
             <div class="card-block">
                 <h6 class="m-b-20">Revenue</h6>
@@ -25,57 +69,17 @@
                 <p class="m-b-0">This Month<span class="f-right">$5,032</span></p>
             </div>
         </div>
-    </div>
+    </div> -->
     <div class="col-md-6 col-xl-3">
         <div class="card bg-c-pink order-card">
             <div class="card-block">
                 <h6 class="m-b-20">Total Profit</h6>
-                <h2 class="text-right"><i class="ti-wallet f-left"></i><span>$9,562</span></h2>
-                <p class="m-b-0">This Month<span class="f-right">$542</span></p>
+                <h2 class="text-right"><i class="ti-wallet f-left"></i><span><?php echo $all_incomes - $getSumCost; ?></span></h2>
+                <p class="m-b-0">This Month<span class="f-right"><?php echo $totalLastMonthIncome - $getLastMonthCost; ?></span></p>
             </div>
         </div>
     </div>
     <!-- order-card end -->
 
-    <!-- statustic and process start -->
-    <div class="col-lg-8 col-md-12">
-        <div class="card">
-            <div class="card-header">
-                <h5>Statistics</h5>
-                <div class="card-header-right">
-                    <ul class="list-unstyled card-option">
-                        <li><i class="fa fa-chevron-left"></i></li>
-                        <li><i class="fa fa-window-maximize full-card"></i></li>
-                        <li><i class="fa fa-minus minimize-card"></i></li>
-                        <li><i class="fa fa-refresh reload-card"></i></li>
-                        <li><i class="fa fa-times close-card"></i></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="card-block">
-                <canvas id="Statistics-chart" height="200"></canvas>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-4 col-md-12">
-        <div class="card">
-            <div class="card-header">
-                <h5>Customer Feedback</h5>
-            </div>
-            <div class="card-block">
-                <span class="d-block text-c-blue f-24 f-w-600 text-center">365247</span>
-                <canvas id="feedback-chart" height="100"></canvas>
-                <div class="row justify-content-center m-t-15">
-                    <div class="col-auto b-r-default m-t-5 m-b-5">
-                        <h4>83%</h4>
-                        <p class="text-success m-b-0"><i class="ti-hand-point-up m-r-5"></i>Positive</p>
-                    </div>
-                    <div class="col-auto m-t-5 m-b-5">
-                        <h4>17%</h4>
-                        <p class="text-danger m-b-0"><i class="ti-hand-point-down m-r-5"></i>Negative</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 </div>
